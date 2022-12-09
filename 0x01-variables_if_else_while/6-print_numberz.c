4a2d25902fe98c692f53f7dc64c5b1d5b5f95831
Yishak <yiseyoni2021@gmail.com>
2022-12-09 13:02:28 -0800

    6 6-print_numberze

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entrry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		putchar(num + '0');
+	putchar('\n');
+	return (0);
+}
