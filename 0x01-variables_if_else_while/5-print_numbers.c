a590d183a31e41af178b5e025a87d825d9239fb3
Yishak <yiseyoni2021@gmail.com>
2022-12-09 12:55:58 -0800

    5-print_number

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; num < 10; num++)
-		printf("%d", num);
+	for (number = 0; number < 10; number++)
+		printf("%d", number);
 	putchar('\n');
 	return (0);
 }
