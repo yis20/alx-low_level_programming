e8b8efaa8f5d768611c8eaf1e065b829a4db8074
Yishak <yiseyoni2021@gmail.com>
2022-12-09 14:32:34 -0800

    task 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,20 +11,20 @@ int main(void)
 	int o;
 	int r;
 
-	for (f = 36; f < 46; f++)
+	for (f = 49; f < 59; f++)
 	{
-		for (o = 36; o < 46; o++)
+		for (o = 49; o < 59; o++)
 		{
 			if (o > f)
 			{
-				for (r = 36; r < 46; r++)
+				for (r = 49; r < 59; r++)
 				{
 					if (r > o)
 					{
 					putchar(f);
 					putchar(o);
 					putchar(r);
-					if (f == 43 && o == 44 && r == 45)
+					if (f == 56 && o == 57 && r == 58)
 					{
 						break;
 					}
