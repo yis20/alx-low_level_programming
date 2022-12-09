c5cc4d6bb302ecd80366e0e34c6ad2f67e6ce755
Yishak <yiseyoni2021@gmail.com>
2022-12-09 13:54:32 -0800

    task 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,13 @@ int main(void)
 	int tenses;
 	int oneses;
 
-	for (tenses = 0; tenes <= 9; tenses++)
+	for (tenses = 0; tenses <= 9; tenses++)
 	{
-		for (oneses = tenses + 1; ones <= 9; oneses++)
+		for (oneses = tenses + 1; oneess <= 9; oneses++)
 		{
 			putchar(tenses + '0');
 			putchar(oneses + '0');
-			if (tens < 88)
+			if (tenses < 88)
 			{
 				putchar(',');
 				putchar(' ');
