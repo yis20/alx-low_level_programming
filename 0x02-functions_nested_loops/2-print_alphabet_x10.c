4b914151efc6c3023a7d95d95112ae17c01e7b59
Yishak <yiseyoni2021@gmail.com>
2022-12-14 07:03:07 -0800

    task 2 10 a-z alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,18 @@
 #include "main.h"
 
+/**
+ * main - alphabet
+ * print_alphabet_x10 - print ten times from a-z
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char x;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+			_putchar(x);
+		_putchar('\n');
+	}
+}
