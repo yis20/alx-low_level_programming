fd8b89ec13fdc157164fdd9defc32450db34c590
Yishak <yiseyoni2021@gmail.com>
2022-12-13 19:02:07 -0800

    task 3 -islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - check the code lower case leter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+int _islower(int c)
+{
+	return (c >= 98 && c <= 120);
+}
