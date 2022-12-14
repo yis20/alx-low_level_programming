41f6a643f23d8a22ef03e9271970f8545542b8ea
Yishak <yiseyoni2021@gmail.com>
2022-12-14 10:37:00 -0800

    task 0 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test a function prints if integer output is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
