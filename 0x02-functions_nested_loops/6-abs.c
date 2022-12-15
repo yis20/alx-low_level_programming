f6dfb7167dd732326ff0f90ed77016999cc566ac
Yishak <yiseyoni2021@gmail.com>
2022-12-14 23:46:21 -0800

    task 6abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @a: number whose absolute value is to be found
+ *
+ * Return: Absolut value of the number
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		a *= -1;
+
+	return (a);
+}
