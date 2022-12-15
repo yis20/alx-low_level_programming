ba635d86009c3e42c9880d926cedabce180a6c23
Yishak <yiseyoni2021@gmail.com>
2022-12-15 00:57:02 -0800

    task 8 24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer _ prints every minute of the day of jack bauer, \starting from0arting from 00:00 to 23:59
+ *
+ * Return: nothing on success
+ */
+void jack_bauer(void)
+{
+	int s = 0, e = 23, s2 = 0, e2 = 59;
+
+	while (s <= e)
+	{
+		while (s2 <= e2)
+		{
+			_putchar(s < 9 ? 0 + '0' : s / 10 + '0');
+			_putchar(s < 9 ? s + '0' : s % 10 + '0');
+			_putchar(':');
+			_putchar(s2 < 9 ? 0 + '0' : s2 / 10 + '0');
+			_putchar(s2 < 9 ? s2 + '0' : s2 % 10 + '0');
+			_putchar('\n');
+
+			s2++;
+		}
+		s2 = 0;
+		s++;
+	}
+}
