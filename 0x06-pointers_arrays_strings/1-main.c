df320a69e1b241b445668c7da8a9f0c2924578ca
Yishak <yiseyoni2021@gmail.com>
2022-12-22 06:34:43 -0800

    man strncat a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	char *ptr;
+
+	printf("%s\n", s1);
+	printf("%s", s2);
