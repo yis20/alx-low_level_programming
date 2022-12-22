38cd563638fce01917361c1911aac88212a4ad69
Yishak <yiseyoni2021@gmail.com>
2022-12-22 06:55:58 -0800

    man strcmp a function that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: s1[y] - s2[y]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int y;
+	y = 0;
+	while (s1[y] != '\0' && s2[y] != '\0')
+	{
+		if (s1[y] != s2[y])
+		{
+			return (s1[y] - s2[y]);
+		}
+		y++;
+	}
+	return (0);
+}
