86e1afa6f98e395a3226e692ecacc5d9f96bb65a
Yishak <yiseyoni2021@gmail.com>
2022-12-23 04:32:29 -0800

    add code

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,12 +4,14 @@
  * main - alphabet.
  *
  *  print_alphabet_x10 - print ten times from a-z.
+ *
+ * Return: 0 success
  */
 void print_alphabet_x10(void);
-{
+
 int i;
 char x;
-
+{
 for (i = 0; i <= 9; i++)
 {
 for (x = 'a'; x <= 'z'; x++)
