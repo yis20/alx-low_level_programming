a47552c88e6583f6fcb364022154ac48346ed14a
Yishak <yiseyoni2021@gmail.com>
2023-01-02 14:22:58 -0800

    task 8 print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,22 @@
 #include "main.h"
+#include <unistd.h>
 /**
  * print_chessboard - function which prints the chessboard.
  * @a: array of pointers.
  *
  * Retuurn: void.
  */
+int _putchar(int c);
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 7; j++)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
-		 _putchar(10);
+		 _putchar('\n');
 	}
 }
