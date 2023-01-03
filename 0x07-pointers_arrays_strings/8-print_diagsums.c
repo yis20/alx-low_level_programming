c2dc56336f9da4c86d2ba11f74dabe8a3c60d9d5
Yishak <yiseyoni2021@gmail.com>
2023-01-02 16:23:02 -0800

    101 task password crackme

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,40 +9,11 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int sum1 = 0, sum2 = 0;
-		for (int i = 0; i < size; i++)
+	int b, sum1 = 0, sum2 = 0;
+
+	for (b = 0; b < size; b++)
 	{
-		sum1 += a[i * size + i];
-		sum2 += a[i * size + (size - i - 1)];
+		sum1 += a[(size + 1) * b];
+		sum2 += a[(size - 1) * (b + 1)];
 	}
 
-	int num_digits = 0;
-	int num = sum1;
-		while (num > 0)
-	{
-		num /= 10;
-		num_digits++;
-	}
-	for (int i = num_digits - 1; i >= 0; i--)
-	{
-		int digit = sum1 / (int)pow(10, i);
-			_putchar(digit + '0');
-		sum1 -= digit * (int)pow(10, i);
-	}
-	_putchar(',');
-	_putchar(' ');
-	num_digits = 0;
-	num = sum2;
-	while (num > 0)
-	{
-		num /= 10;
-		num_digits++;
-	}
-	for (int i = num_digits - 1; i >= 0; i--)
-	{
-		int digit = sum2 / (int)pow(10, i);
-			_putchar(digit + '0');
-		sum2 -= digit * (int)pow(10, i);
-	}
-	_putchar('\n');
-}
