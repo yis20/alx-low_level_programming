ca7d5ad84fd3a2e85c0772af96f8e4fe2b0a28d7
Yishak <yiseyoni2021@gmail.com>
2023-01-06 08:57:54 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - the name of the program itself
+ * @argv - is an array of strings that holds the arguments passed to the program
+ * @argc - is an integer that holds the number of arguments passed to the program, including the name of the program itself.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	 return 0;
+}
