2649108039ccf9f0faa58bcf3aea731dca661ef6
Yishak <yiseyoni2021@gmail.com>
2023-01-06 09:49:49 -0800

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-/*
- * main - the name of the program itself
- * @argv - is an array of strings that holds the arguments passed to the program
- * @argc - is an integer that holds the number of arguments passed to the program, including the name of the program itself.
+/**
+ * main - this is a function to print the name of the program itself
+ * @argv: is an array of strings ie holds the arguments passed to the program
+ * @argc: is an integer that holds the no of arguments passed to the program,
+ * including the name of the program itself.
+ * Return: 0(success)
  */
 int main(int argc, char *argv[])
 {
 	printf("%d\n", argc - 1);
 	(void)argv;
-	 return 0;
+	return (0);
 }
