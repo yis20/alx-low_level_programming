1b623a67578532259481b08d9be72a95987c2b33
Yishak <yiseyoni2021@gmail.com>
2023-01-11 08:37:13 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * print_times_table - prints the times table for n.
+ * print_times_table - prints the n times table for n, starting with 0.
  * @n: The multiplication table requested.
  * Return: Nothing.
  */
 void print_times_table(int n)
 {
-	int i, j, res;
+	int i, j, xy;
 
 	if (!(n > 15 || n < 0))
 	{
@@ -14,32 +14,32 @@ void print_times_table(int n)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				res = (i * j);
+				xy = (i * j);
 				if (j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 				}
-				if (res < 10 && j != 0)
+				if (xy < 10 && j != 0)
 				{
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((res % 10) + '0');
+					_putchar((xy % 10) + '0');
 				}
-				else if (res >= 10 && res < 100)
+				else if (xy >= 10 && xy < 100)
 				{
 					_putchar(' ');
-					_putchar((res / 10) + '0');
-					_putchar((res % 10) + '0');
+					_putchar((xy / 10) + '0');
+					_putchar((xy % 10) + '0');
 				}
-				else if (res >= 100 && j != 0)
+				else if (xy >= 100 && j != 0)
 				{
-					_putchar((res / 100) + '0');
-					_putchar((res / 10) % 10 + '0');
-					_putchar((res % 10) + '0');
+					_putchar((xy / 100) + '0');
+					_putchar((xy / 10) % 10 + '0');
+					_putchar((xy % 10) + '0');
 				}
 				else
-					_putchar((res % 10) + '0');
+					_putchar((xy % 10) + '0');
 			}
 			_putchar('\n');
 		}
