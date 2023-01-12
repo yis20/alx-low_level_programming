7cd124c273ef92a81b79437b454c8de94bcef6a2
Yishak <yiseyoni2021@gmail.com>
2023-01-12 05:03:04 -0800

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,17 +4,19 @@
 * _calloc - allocates memory for an initialized buffer
   * @nmemb: number of memory bytes
   * @size: number of data type bits
-  * Return: return pointer to initialized buffer
+  * Return: return pointer to initialized buffeir
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	int *p;
 	unsigned int i;
 
-	p = malloc(nmemb * size);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(size * nmemb);
 	if (p == NULL)
-	return (NULL);
-	for (i = 0; i < nmemb * size; i++)
-	((char *)p)[i] = 0;
+		return (NULL);
+	for (i = 0; i < nmemb ; i++)
+		p[i] = 0;
 	return (p);
 }
