735848abfb951ee2e5eefa50844627e5302d510c
Yishak <yiseyoni2021@gmail.com>
2023-01-12 12:02:29 -0800

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
 
+/**
+ * _calloc - Allocates memory space and returns a pointer to that direction
+ * @nmemb: Number of elements to store
+ * @size: Data type of the elements
+ * Return: Pointer to the allocated memory space
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(size * nmemb);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb ; i++)
+		p[i] = 0;
+	return (p);
+}
