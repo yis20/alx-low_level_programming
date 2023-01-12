7713f7a73658aa39e2585ba4de4726ca2e689b22
Yishak <yiseyoni2021@gmail.com>
2023-01-11 23:52:13 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - draws a square using multiple terminal lines and #'s.
+ * @size: the number of # to print in each line.
+ * Return: nothing
+ */
+void print_square(int n)
+{
+	int x, y;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < n; y++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
