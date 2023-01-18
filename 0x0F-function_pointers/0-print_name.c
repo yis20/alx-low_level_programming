f79c63974002a4b6b05d0b653dcf344e862b21ff
Yishak <yiseyoni2021@gmail.com>
2023-01-18 01:41:14 -0800

    what is my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+*print_name - print the name of a person according to specified function
+*@name: name to print
+*@f: pointer to function
+*Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		{
+		f(name);
+	}
+}
