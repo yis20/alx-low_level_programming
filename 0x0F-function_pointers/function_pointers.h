0d8573c01adbd8b1fff4a431bf52f6cad20eeac5
Yishak <yiseyoni2021@gmail.com>
2023-01-18 12:19:01 -0800

    100-main_opcodes.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,6 @@
 #include <stdlib.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int (*get_op_func(char *s))(int, int);
 int int_index(int *array, int size, int (*cmp)(int));
 #endif
