f30acc9460507f6f6dc268a65e1ead01b4c40fa0
Yishak <yiseyoni2021@gmail.com>
2023-01-18 22:40:54 -0800

    removes the lib<stdarg.h>

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef _HEADER
+#define _HEADER
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
