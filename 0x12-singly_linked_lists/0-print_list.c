3920468154d706dd6839919292a423c3b08667c8
Yishak <yiseyoni2021@gmail.com>
2023-01-26 10:04:23 -0800

    singlyLinkedLists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+*print_list - print the contents of a single linked list
+*@h: head node of the list
+*Return: number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,5 @@
+Introduction to Data Structures and Algorithm in C.
+
+A Single Linked List is linear data structure in which the elements are not stored in contiguous memory locations. This data structure helps to overcome the various limitations of an array.
+
+
