2448ad795ffd8691cc20b3f915fb682ff9516292
Yishak <yiseyoni2021@gmail.com>
2023-01-31 10:45:20 -0800

    listsafe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,33 +1,60 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * print_listint_safe - A function that prints the elementsin a  list
- * @head: A pointer to listint_t structure
- * Return: The number of nodes. Exits with 98 on failure
+ * free_aux - frees a linked list
+ * @p: head of a list.
+ *
+ * Return: no return.
+ */
+void free_aux(aux_list *p)
+{
+	aux_list *aux;
+
+	if (p == NULL)
+	{
+		return;
+	}
+	while (p != NULL)
+	{
+		aux = p;
+		p = p->next;
+		free(aux);
+	}
+}
+
+/**
+ * print_listint_safe - prints a linked list.
+ * @head: head of a list.
+ *
+ * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes = 0;
-	const listint_t *one = head, *two = head;
+	size_t c = 0;
+	aux_list *a, *b, *t;
 
-	if (head == NULL)
-		exit(98);
-
-	while (one && two && two->next && head)
+	a = NULL;
+	for (; head != NULL; c++, head = head->next)
 	{
-		one = one->next;
-		two = two->next->next;
-		if (one == two)
-		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
+		t = malloc(sizeof(aux_list));
+		if (!t)
 			exit(98);
+		t->p = (void *)head;
+		t->next = a;
+		a = t;
+		b = a->next;
+		while (b != NULL)
+		{
+			if (head == b->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_aux(a);
+				return (c);
+			}
+			b = b->next;
 		}
-
 		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		nodes++;
 	}
-	head = NULL;
-	return (nodes);
+	free_aux(a);
+	return (c);
 }
