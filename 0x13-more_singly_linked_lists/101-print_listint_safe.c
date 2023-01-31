c4e42d9a1d6792340e315a9d80082cdf6d1e4ece
Yishak <yiseyoni2021@gmail.com>
2023-01-31 10:51:49 -0800

    print refresh

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,34 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
-* print_listint_safe - print list consecutively in memory
-* @head: pointer to first element
-*
-* Return: count of nodes
-*/
+ * print_listint_safe - function that prints a linked list
+ * @head: pointer to the head
+ *
+ * Return: number of nodes in the list
+ */
 
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	int sub;
+	listint_t *aux = (void *)head, *test = 0;
+	int flag = 0;
 
-	while (head)
+	while (aux != NULL)
 	{
-		count++;
-
-		printf("[%p] %d\n", (void *)head, head->n);
-
-		sub = head - head->next;
-
-		if (sub > 0)
-			head = head->next;
-
-		else
+		if (flag == 0)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
+			test = aux;
+			printf("[%p] %d\n", (void *)aux, aux->n);
 		}
-
+		if (flag == 1)
+		{
+			if (test <= aux)
+			{
+				printf("-> [%p] %d\n", (void *)aux, aux->n);
+				return (count);
+			}
+			printf("[%p] %d\n", (void *)aux, aux->n);
+		}
+		count += 1;
+		test = aux;
+		aux = aux->next;
+		flag = 1;
 	}
 	return (count);
 }
