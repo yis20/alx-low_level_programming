3e71c23ecc311689545b7bd13211f1487266079a
Yishak <yiseyoni2021@gmail.com>
2023-02-02 13:55:39 -0800

    5 bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,25 +1,24 @@
-#include <stdlib.h>
-#include "holberton.h"
-
+#include "main.h"
 /**
-  * flip_bits - ...
-  * @n: ...
-  * @m: ...
-  *
-  * Return: ...
-  */
+ * flip_bits - function that returns the number of bits you would need
+ * to flip to get from one number to another.
+ * @n: Value decimal to decimal
+ * @m: Value decimal to decimal
+ *
+ * Return: values
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int mask = 0, count = 0;
+	int i = 0;
+	unsigned long int count;
 
-	mask = n ^ m;
-	while (mask)
+	count = n ^ m;
+
+	while (count)
 	{
-		if (mask & 1)
-			count++;
-
-		mask >>= 1;
+		i += count & 1;
+		count = count >> 1;
 	}
 
-	return (count);
+	return (i);
 }
