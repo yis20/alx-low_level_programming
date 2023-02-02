2bc8520e4fd1d48c09e966a485e817d456fa0e73
Yishak <yiseyoni2021@gmail.com>
2023-02-02 05:33:00 -0800

    bits

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * binary_to_uint - A finction that converts binary number
- * to an unsigned integer
- * @b: A pointer to binary string
- * Return: unsigned integer converted number
- */
+  * binary_to_uint - Converts a binary number to an unsigned int
+  * @b: The binary string to converts
+  *
+  * Return: The positive number converted from a binary
+  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int base = 1, result = 0, len = 0;
+	unsigned int len = 0, count = 0, sum = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[len])
-		len++;
-
-	while (len)
+	len = _strlen(b);
+	while (len--)
 	{
-		if (b[len - 1] != '0' && b[len - 1] != '1')
+		if (b[len] != 48 && b[len] != 49)
 			return (0);
 
-		if (b[len - 1] == '1')
-			result += base;
-		base *= 2;
-		len--;
+		if (b[len] == 49)
+			sum += 1 << count;
+
+		count++;
 	}
-	return (result);
+
+	return (sum);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
 }
